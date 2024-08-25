5763c79438d4e0a6d4b4eb67df694b8feff2c981
Badal Singh <badal1443@gmail.com>
2024-08-25 11:02:15 -0700

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import mongodbconnect as mc
 
 app = Flask(__name__)
 
 @app.route("/")
-def my_app():
-    return "Hello Flask, My first app"
+def home():
+    home_data={"page":"home"}
+    return jsonify(home_data)
+ 
+@app.route("/get-order/<order_id>")
+def get_order(order_id):
+    return jsonify(mc.get_order(order_id))
+
+@app.route("/add-user", methods=['POST'])
+def addOrder():
+    order=request.json
+    mc.create_new_order(order)
+    return jsonify(order)
+
+if __name__=="__main__":
+    app.run(debug=True,host="0.0.0.0", port="5000")
