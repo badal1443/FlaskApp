2e23e578e7c508ffe2ca71361540e40b84d122c5
Badal Singh <badal1443@gmail.com>
2024-08-25 11:03:40 -0700

    Create mongodbconnect.py

---
+++ mongodbconnect.py
@@ -0,0 +1,48 @@
+
+from pymongo.mongo_client import MongoClient
+import certifi
+import os
+from bson import json_util
+import json
+from flask import jsonify
+
+#uri = os.getenv('MONGO_DB_CONN')
+uri=os.getenv('MONGO_DB_CONN')
+
+#print(os.getenv('MONGO_DB_CONN'))
+# Create a new client and connect to the server
+client = MongoClient(uri,tlsCAFile=certifi.where())
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+
+    db=client["SQLAuthority"]
+    collection=db.get_collection("neworder")
+    print(collection.find_one({"orderno":"123"}))
+except Exception as e:
+    print(e)
+
+#order={"item":"Chicken curry","orderno":5557,"category":"Main"}
+
+def get_order(orderid):
+    try:
+        data=json_util.dumps(collection.find_one({'orderno':orderid}))
+        return json.loads(data)
+    except Exception as e:
+        print(f"exception in finding order {orderid}")
+        print(e)
+
+
+def create_new_order(orderDict):
+    try:
+        result=collection.insert_one(jsonify(orderDict))
+        print(result)
+        
+        return json.loads(json_util.dumps(result))
+    except Exception as e:
+        print("exception in creating new order")
+        print(e)
+#get_user("555")
+#create_new_order(order)
