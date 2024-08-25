2724ed7fc22fa10c37a615e48208c94880aba8a5
Badal Singh <badal1443@gmail.com>
2024-08-25 11:58:52 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import mongodbconnect as mc
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    #home_data={"page":"home"}
+    #return jsonify(home_data)
+    print('Request for index page received')
+    return render_template('index.html')
+ 
+@app.route("/get-order/<order_id>")
+def get_order(order_id):
+    return jsonify(mc.get_order(order_id))
+
+@app.route("/add-user", methods=['POST'])
+def addOrder():
+    order=request.json
+    mc.create_new_order(order)
+    return jsonify(order)
+
+if __name__=="__main__":
+    app.run()
